255164292bde00cb1590fdc738b28ee7baa07fab
ar-mine <934218777@qq.com>
2022-08-23 16:39:57 +0800

    Add buffer function for recording

--- isaacgymenvs/drl_custmaized/simple_pg.py
+++ isaacgymenvs/drl_custmaized/simple_pg.py
@@ -1,23 +1,91 @@
 import torch
 import torch.nn as nn
-from torch.distributions.categorical import Categorical
 from torch.optim import Adam
 import numpy as np
 import gym
 from gym.spaces import Discrete, Box
 from typing import Optional, List, Tuple
+from models import MLPCategoricalActor, combined_shape, discount_cumsum
+import hydra
+
+
+class SPGBuffer:
+    """
+       A buffer for storing trajectories experienced by a VPG agent interacting
+       with the environment, and using Generalized Advantage Estimation (GAE-Lambda)
+       for calculating the advantages of state-action pairs.
+       """
+
+    def __init__(self, obs_dim, act_dim, size, gamma=0.99):
+        # For discrete space, act_dim = 1(classification)
+        act_dim = 1
+        self.obs_buf = np.zeros(combined_shape(size, obs_dim), dtype=np.float32)
+        self.act_buf = np.zeros(combined_shape(size, act_dim), dtype=np.float32)
+        self.rew_buf = np.zeros(size, dtype=np.float32)
+        self.ret_buf = np.zeros(size, dtype=np.float32)
+        self.logp_buf = np.zeros(size, dtype=np.float32)
+        self.gamma = gamma
+        self.ptr, self.path_start_idx, self.max_size = 0, 0, size
+
+    def store(self, obs, act, rew, logp):
+        """
+        Append one timestep of agent-environment interaction to the buffer.
+        """
+        assert self.ptr < self.max_size  # buffer has to have room so you can store
+        self.obs_buf[self.ptr] = obs
+        self.act_buf[self.ptr] = act
+        self.rew_buf[self.ptr] = rew
+        self.logp_buf[self.ptr] = logp
+        self.ptr += 1
+
+    def finish_path(self, last_val=0):
+        """
+        Call this at the end of a trajectory, or when one gets cut off
+        by an epoch ending. This looks back in the buffer to where the
+        trajectory started, and uses rewards and value estimates from
+        the whole trajectory to compute advantage estimates with GAE-Lambda,
+        as well as compute the rewards-to-go for each state, to use as
+        the targets for the value function.
+
+        The "last_val" argument should be 0 if the trajectory ended
+        because the agent reached a terminal state (died), and otherwise
+        should be V(s_T), the value function estimated for the last state.
+        This allows us to bootstrap the reward-to-go calculation to account
+        for timesteps beyond the arbitrary episode horizon (or epoch cutoff).
+        """
+        path_slice = slice(self.path_start_idx, self.ptr)
+        rews = np.append(self.rew_buf[path_slice], last_val)
+
+        # the next line computes rewards-to-go, to be targets for the value function
+        # self.ret_buf[path_slice] = np.sum(rews[:-1])
+        self.ret_buf[path_slice] = discount_cumsum(rews, self.gamma)[:-1]
+
+        self.path_start_idx = self.ptr
+
+    def get(self):
+        """
+        Call this at the end of an epoch to get all of the data from
+        the buffer, with advantages appropriately normalized (shifted to have
+        mean zero and std one). Also, resets some pointers in the buffer.
+        """
+        assert self.ptr == self.max_size  # buffer has to be full before you can get
+        self.ptr, self.path_start_idx = 0, 0
+        data = dict(obs=self.obs_buf, act=self.act_buf, ret=self.ret_buf, logp=self.logp_buf)
+        return {k: torch.as_tensor(v, dtype=torch.float32) for k, v in data.items()}
 
 
 class SimplePolicyGradient:
-    def __init__(self):
+    def __init__(self, cfg):
+        self.cfg = cfg
+
         # Will be loaded from cfg later
-        self.env_name = 'CartPole-v1'
-        self.hidden_sizes = [32]
-        self.lr = 1e-2
-        self.max_epochs = 50
-        self.batch_size = 5000
-        self.render = True
-        self.device = torch.device("cuda:0")
+        self.env_name = self.cfg['env_name']
+        self.hidden_sizes = self.cfg['hidden_sizes']
+        self.lr = self.cfg['lr']
+        self.max_epochs = self.cfg['max_epochs']
+        self.batch_size = self.cfg['batch_size']
+        self.render = self.cfg['render']
+        self.device = torch.device(self.cfg['device'])
 
         # Env loader
         self.env = None
@@ -27,7 +95,9 @@ class SimplePolicyGradient:
 
         # Network
         # make core of policy network
-        self.network = self.mlp(sizes=[self.obs_dim] + self.hidden_sizes + [self.act_dim])
+        self.actor, self.buffer = self.create_estimator()
+
+        self.pi_optimizer = Adam(self.actor.parameters(), lr=self.lr)
 
     def create_env(self):
         # make environment, check spaces, get obs / act dims
@@ -41,19 +111,21 @@ class SimplePolicyGradient:
         self.obs_dim = self.env.observation_space.shape[0]
         self.act_dim = self.env.action_space.n
 
-    def get_policy(self, obs):
-        # make function to compute action distribution
-        act_prob = self.network(obs)
-        return Categorical(logits=act_prob)
+    def create_estimator(self):
+        return MLPCategoricalActor(self.obs_dim, self.act_dim, self.hidden_sizes, nn.Tanh, device=self.device), \
+               SPGBuffer(self.obs_dim, self.act_dim, self.batch_size)
 
-    def get_action(self, obs):
-        # make action selection function (outputs int actions, sampled from policy)
-        return self.get_policy(obs).sample().item()
+    def compute_loss_pi(self, data):
+        obs, act, logp_old, ret = data['obs'].to(self.device), data['act'].to(self.device), \
+                                  data['logp'].to(self.device), data['ret'].to(self.device)
 
-    def compute_loss(self, obs, act, weights):
+        # Policy loss
+        pi, logp = self.actor(obs, act.squeeze())
+
         # make loss function whose gradient, for the right data, is policy gradient
-        log_prob = self.get_policy(obs).log_prob(act)
-        return -(log_prob * weights).mean()
+        loss_pi = -(logp * ret).mean()
+
+        return loss_pi
 
     @staticmethod
     def reward_to_go(rewards):
@@ -64,92 +136,72 @@ class SimplePolicyGradient:
         return rtgs
 
     # for training policy
-    def train_one_epoch(self, optimizer) -> (Tuple, List, List):
+    def train_one_epoch(self) -> (Tuple, List, List):
         # make some empty lists for logging.
-        batch_obs = []  # for observations
-        batch_acts = []  # for actions
-        batch_weights = []  # for R(tau) weighting in policy gradient
         batch_rets = []  # for measuring episode returns
         batch_lens = []  # for measuring episode lengths
 
         # reset episode-specific variables
-        obs = self.env.reset()  # first obs comes from starting distribution
-        done = False  # signal from environment that episode is over
-        ep_rewards = []  # list for rewards accrued throughout ep
-
-        # render first episode of each epoch
-        finished_rendering_this_epoch = False
+        obs, done, ep_ret, ep_len = self.env.reset(), False, 0, 0  # first obs comes from starting distribution
 
         # collect experience by acting in the environment with current policy
-        while True:
+        for t in range(self.batch_size):
             # rendering
-            if (not finished_rendering_this_epoch) and self.render:
+            if self.render:
                 self.env.render()
 
-            # save obs
-            batch_obs.append(obs.copy())
-
             # act in the environment
-            act = self.get_action(torch.as_tensor(obs, dtype=torch.float32))
-            obs, reward, done, _ = self.env.step(act)
+            act, logp = self.actor.step(torch.as_tensor(obs, dtype=torch.float32, device=self.device))
+            act, logp = act.cpu().numpy(), logp.cpu().numpy()
+            obs_next, reward, done, _ = self.env.step(act)
+            ep_ret += reward
+            ep_len += 1
 
-            # save action, reward
-            batch_acts.append(act)
-            ep_rewards.append(reward)
+            # save obs, action, reward
+            self.buffer.store(obs, act, reward, logp)
 
-            if done:
-                # if episode is over, record info about episode
-                ep_ret, ep_len = sum(ep_rewards), len(ep_rewards)
+            # Update obs
+            obs = obs_next
+
+            epoch_ended = t == self.batch_size - 1
+
+            if done or epoch_ended:
                 batch_rets.append(ep_ret)
                 batch_lens.append(ep_len)
 
-                # # the weight for each logprob(a|s) is R(tau)
-                # batch_weights += [ep_ret] * ep_len
-                # the weight for each logprob(a_t|s_t) is reward-to-go from t
-                batch_weights += list(self.reward_to_go(ep_rewards))
+                self.buffer.finish_path()
 
                 # reset episode-specific variables
-                obs, done, ep_rewards = self.env.reset(), False, []
-
-                # won't render again this epoch
-                finished_rendering_this_epoch = True
-
-                # end experience loop if we have enough of it
-                if len(batch_obs) > self.batch_size:
-                    break
+                obs, done, ep_ret, ep_len = self.env.reset(), False, 0, 0  # first obs comes from starting distribution
 
         # take a single policy gradient update step
-        optimizer.zero_grad()
-        batch_loss = self.compute_loss(obs=torch.as_tensor(batch_obs, dtype=torch.float32),
-                                       act=torch.as_tensor(batch_acts, dtype=torch.int32),
-                                       weights=torch.as_tensor(batch_weights, dtype=torch.float32)
-                                       )
-        batch_loss.backward()
-        optimizer.step()
-        return batch_loss, batch_rets, batch_lens
+        batch_loss = self.opt_update()
+
+        return batch_loss.cpu(), batch_rets, batch_lens
+
+    def opt_update(self):
+        # take a single policy gradient update step
+        data = self.buffer.get()
+
+        # Train policy with a single step of gradient descent
+        self.pi_optimizer.zero_grad()
+        loss_pi = self.compute_loss_pi(data)
+        loss_pi.backward()
+        self.pi_optimizer.step()
+
+        return loss_pi
 
     def train(self):
-        # make optimizer
-        optimizer = Adam(self.network.parameters(), lr=self.lr)
-
         # training loop
         for i in range(self.max_epochs):
-            batch_loss, batch_rets, batch_lens = self.train_one_epoch(optimizer)
+            batch_loss, batch_rets, batch_lens = self.train_one_epoch()
             print('epoch: %3d \t loss: %.3f \t return: %.3f \t ep_len: %.3f' %
                   (i, batch_loss, np.mean(batch_rets).item(), np.mean(batch_lens).item()))
 
-    @staticmethod
-    def mlp(sizes, activation=nn.Tanh, output_activation=nn.Identity):
-        # Build a feedforward neural network.
-        layers = []
-        for j in range(len(sizes) - 1):
-            act = activation if j < len(sizes) - 2 else output_activation
-            layers += [nn.Linear(sizes[j], sizes[j + 1]), act()]
-        return nn.Sequential(*layers)
 
-
-def main():
-    agent = SimplePolicyGradient()
+@hydra.main(version_base=None, config_path=".", config_name="config")
+def main(cfg):
+    agent = SimplePolicyGradient(cfg)
     agent.train()
 
 
---
+++ isaacgymenvs/drl_custmaized/vpg.py
@@ -0,0 +1,18 @@
+import hydra
+from simple_pg import SimplePolicyGradient
+
+
+class VPG(SimplePolicyGradient):
+    def __init__(self, cfg):
+        self.cfg = cfg
+        super().__init__(cfg)
+
+
+@hydra.main(version_base=None, config_path=".", config_name="config")
+def main(cfg):
+    agent = SimplePolicyGradient(cfg)
+    agent.train()
+
+
+if __name__ == "__main__":
+    main()
